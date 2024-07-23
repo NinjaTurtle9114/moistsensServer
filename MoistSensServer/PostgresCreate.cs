dd29328fa2ee7eefc9f3893c416b7aecad42d44c
Simon <simon.parent.8@ulaval.ca>
2024-07-23 17:07:00 -0400

    WIP: create class for Postgre

---
+++ MoistSensServer/PostgresCreate.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Npgsql;
+namespace MoistSensServer;
+
+
+public class PostgresCreate
+{
+    private static string Server = "localhost";
+    private static string User = "postgres";
+    private static string Database = "PostgreDB";
+    private static string Port = "5432";
+    private static string Password = "si_so*LtYoFR2dDFy@-s";
+
+    static void Main(string[] args)
+    {
+        string connectionString = String.Format(
+            "Server={0};Username={1};Database={2};Port={3};Password={4};SSLMode=Prefer",
+            Server,
+            User,
+            Database,
+            Port,
+            Password);
+    }
+}
